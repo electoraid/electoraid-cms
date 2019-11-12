6d1878e6531dde607e1d867806753d75dfb028b5
David Eads <davideads@gmail.com>
2019-11-12 16:34:17 -0600

    expand what we can query through the graphql api

--- elections/schema.py
+++ elections/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django.types import DjangoObjectType
-from .models import Person, Candidacy, PoliticalActionCommittee
+from .models import *
 
 
 class PersonType(DjangoObjectType):
@@ -13,14 +13,39 @@ class CandidacyType(DjangoObjectType):
         model = Candidacy
 
 
+class OfficeTenureType(DjangoObjectType):
+    class Meta:
+        model = OfficeTenure
+
+
 class PoliticalActionCommitteeType(DjangoObjectType):
     class Meta:
         model = PoliticalActionCommittee
+
+
+class OfficeType(DjangoObjectType):
+    class Meta:
+        model = Office
+
+
+class ElectionType(DjangoObjectType):
+    class Meta:
+        model = Election
+
+
+class PoliticalBodyType(DjangoObjectType):
+    class Meta:
+        model = PoliticalBody
+
 
 class Query(object):
     all_people = graphene.List(PersonType)
     all_candidacies = graphene.List(CandidacyType)
     all_political_action_committees = graphene.List(PoliticalActionCommitteeType)
+    all_office_tenures = graphene.List(OfficeTenureType)
+    all_offices = graphene.List(OfficeType)
+    all_elections = graphene.List(ElectionType)
+    all_political_bodies = graphene.List(PoliticalBodyType)
 
     def resolve_all_people(self, info, **kwargs):
         return Person.objects.select_related('candidacies').all()
@@ -30,3 +55,6 @@ class Query(object):
 
     def resolve_all_political_action_committees(self, info, **kwargs):
         return PoliticalActionCommittee.objects.all()
+
+    def resolve_all_office_tenures(self, info, **kwargs):
+        return OfficeTenure.objects.all()
