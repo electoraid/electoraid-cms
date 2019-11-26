8fdc3f0de5872bd4c1e6b6d661b77201f35183a7
David Eads <davideads@gmail.com>
2019-11-25 22:57:14 -0600

    add data loader from campaign finance api

---
+++ ilcampaigncash/management/commands/load_ilcampaigncash.py
@@ -0,0 +1,66 @@
+import requests
+from django.core.management.base import BaseCommand, CommandError
+from django.db.utils import IntegrityError
+from electoraid.models import PoliticalActionCommittee, Person
+
+ILCAMPAIGNCASHURL = 'https://il-campaign-finance-api.herokuapp.com/v1/graphql'
+
+# Uncomment the following to play with caching
+#ILCAMPAIGNCASHURL = 'https://cdn.fastql.io/v1/thechicagoreporter'
+
+
+PERSONQUERY = """
+    {
+        candidates {
+            id
+            first_name
+            last_name
+        }
+    }
+"""
+
+COMMITTEEQUERY = """
+    {
+        committees {
+            id
+            name
+        }
+    }
+"""
+
+class Command(BaseCommand):
+    help = 'Fetch data from the Illinois Campaign Cash API'
+
+    # def add_arguments(self, parser):
+        # parser.add_argument('poll_ids', nargs='+', type=int)
+
+    def run_query(self, query):
+        resp = requests.post(url=ILCAMPAIGNCASHURL, json={'query': query})
+        return resp.json().get('data')
+
+    def load_people(self):
+        data = self.run_query(PERSONQUERY)
+        for candidate in data.get('candidates'):
+            candidate['candidate_id'] = candidate.pop('id')
+            person, created = Person.objects.get_or_create(**candidate)
+            if created:
+                self.stdout.write('created {first_name} {last_name} ({candidate_id})'.format(**candidate))
+            else:
+                self.stdout.write('skipped {first_name} {last_name} ({candidate_id})'.format(**candidate)) 
+        
+    def load_committees(self):
+        data = self.run_query(COMMITTEEQUERY)
+        for committee in data.get('committees'):
+            committee['committee_id'] = committee.pop('id')
+            db_committee, created = PoliticalActionCommittee.objects.get_or_create(**committee)
+            if created:
+                self.stdout.write('created {name} ({committee_id})'.format(**committee))
+            else:
+                self.stdout.write('skipped {name} ({committee_id})'.format(**committee)) 
+
+    def handle(self, *args, **options):
+        self.stdout.write('--- loading committees ---\n')    
+        self.load_committees()
+
+        self.stdout.write('--- loading people ---\n')    
+        self.load_people()
