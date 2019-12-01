d3741b52c27371eab65b820c69dd64ad63cfea10
David Eads <davideads@gmail.com>
2019-12-01 11:51:30 -0600

    rename campaign cash loader module to 'loaders'
    the idea here is to collect all the loaders in one place

--- electoraid_cms/settings.py
+++ electoraid_cms/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'electoraid.apps.ElectoraidConfig',
-    'ilcampaigncash',
+    'electoraid',
+    'electoraid_loaders',
     'reversion',
     'baton.autodiscover',
 ]
