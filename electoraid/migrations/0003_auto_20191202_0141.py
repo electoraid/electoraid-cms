0e4a2c4812e9a6f39642ea17cc141d32821e7baf
David Eads <davideads@gmail.com>
2019-12-01 19:54:47 -0600

    migration surgery

--- electoraid/migrations/0003_auto_20191202_0141.py
+++ electoraid/migrations/0003_auto_20191202_0141.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('electoraid', '0002_auto_20191202_0120'),
+        ('electoraid', '0001_initial'),
     ]
 
     operations = [
--- electoraid_cms/settings.py
+++ electoraid_cms/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'electoraid',
+    'autoslug',
     'electoraid_loaders',
     'reversion',
     'baton.autodiscover',
